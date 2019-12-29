2a1e071f9e82a402e376b5cd454cdafe421b1650
Ted-Majima <Ted-Majima@users.noreply.github.com>
2019-12-29 13:03:35 +0900

    Update IoT.py

--- IoT.py
+++ IoT.py
@@ -6,10 +6,10 @@ import datetime
 from time import sleep
 
 def main():
-    dt_now = datetime.datetime.now()
-    print(dt_now.strftime('%Y/%m/%d %H:%M:%S') + "IoT Device Online")
-    print(os.environ.get('LANG'))
-    #print(os.environ.get('RemoteENV'))
+  dt_now = datetime.datetime.now()
+  print(dt_now.strftime('%Y/%m/%d %H:%M:%S') + "Hello World from IoT device")
+  print(os.environ.get('LANG'))
+  #print(os.environ.get('RemoteENV'))
 
 
 if __name__ == '__main__':
